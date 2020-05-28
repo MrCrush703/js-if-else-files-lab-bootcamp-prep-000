a5dc3f715f3d17c8e99b4c838a58d96d1c01265b
Cornelius A Gregory <businessman7oh3@gmail.com>
2020-05-28 18:28:46 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,6 +4,19 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
+if (index.name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected: Susan, Received: "+index.name)
+}
+
+
+
+if (index.height === 70) {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+index.height)
+}
 
 
 
