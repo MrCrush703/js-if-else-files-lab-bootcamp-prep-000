dadcf5c83e4d6ea31e71b03ad4e39c4abc8eaa4f
Cornelius A Gregory <businessman7oh3@gmail.com>
2020-05-28 18:12:57 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,10 +1,3 @@
-var name = "Susan"
-if (name  === "Susan"){
-  console.log("The name is correct")
-} else{
-  console.log("Expected the name to be Susan")
-}
-
 var name = "Joe"
 var height = "74"
  
